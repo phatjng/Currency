5ea9c8907bf107b41e19ec75658ada2c77117383
phatjng <phatjng@pm.me>
2023-02-06 15:45:45 -0800

    initial commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Jamie Ng on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
